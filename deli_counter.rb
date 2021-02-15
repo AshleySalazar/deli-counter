44c6a1754a4db84b151a27c8f25e60ca402c9152
Ashley Salazar <ashleysalazar0902@gmail.com>
2021-02-14 21:16:39 -0600

    commit tree 66f7161ed6fdba6937e7b9de0e1ca43bcb7debf7

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def line(array)
+    if (array.size > 0)
+        puts "The line is currently: #{array.collect.with_index{ |x, i| "#{i + 1}. #{x}" }.join(" ")}"
+    else
+        puts "The line is currently empty."
+    end
+end
+
+def take_a_number(array, name)
+    array << name
+    puts "Welcome, #{name}. You are number #{array.index(name) + 1} in line."
+end
+
+def now_serving(array)
+    if (array.size > 0)
+        puts "Currently serving #{array[0]}."
+        array.shift
+    else
+        puts "There is nobody waiting to be served!"
+    end
+end
